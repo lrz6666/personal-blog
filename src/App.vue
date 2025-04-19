d326cffb65d50754a56159b3146865c7ba641a3f
lrz6666 <lrz1379795941@gmail.com>
2025-04-19 22:00:43 +0800

    finish main layout

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,42 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="common-layout ">
+    <el-container class="baseLayout">
+      <el-aside width="200px" >
+        <el-scrollbar>
+          <el-menu >
+            <el-menu-item index="1">
+              <template #title>
+                <span>Navigator One</span>
+              </template>
+            </el-menu-item>
+            <el-menu-item index="2">
+              <template #title>
+                <span>Navigator One</span>
+              </template>
+            </el-menu-item>
+            <el-menu-item index="3">
+              <template #title>
+                <span>Navigator One</span>
+              </template>
+            </el-menu-item>
+          </el-menu>
+        </el-scrollbar>
+      </el-aside>
+      <el-container>
+        <el-header style="background-color: yellow;">Header</el-header>
+        <el-main style="background-color: green;">Main</el-main>
+      </el-container>
+    </el-container>
+  </div>
 </template>
 
+
+<script setup lang="ts">
+
+</script>
+
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+.baseLayout {
+  height: 100vh;
 }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
+import 'element-plus/dist/index.css'
 import App from './App.vue'
+import "./assets/main.css"
+const app = createApp(App)
 
-createApp(App).mount('#app')
+app.mount('#app')
