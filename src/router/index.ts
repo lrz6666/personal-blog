f853cb593ed4a67be9aa2411d20a9a935e0d3a38
lrz666 <lrz1379795941@gmail.com>
2025-05-22 00:17:59 +0800

    Route guard has been set up

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,7 +4,7 @@ import UserManageView from "@/views/UserManageView.vue";
 import BlogManageView from "@/views/BlogManageView.vue";
 import component from "element-plus/es/components/tree-select/src/tree-select-option.mjs";
 import LoginView from "@/views/LoginView.vue";
-
+import { verifyToken } from "@/api/users";
 const routes = [
     { path: '/login', name:'Login',component: LoginView, meta:{ requiresAuth: false}},
     { 
@@ -22,4 +22,32 @@ const router = createRouter({
     history: createWebHistory(),
     routes,
   });
+
+  //路由守卫验证token与刷新token
+  router.beforeEach(async (to, from, next) => {
+  const accessToken = localStorage.getItem('access_token');
+
+  // 如果目标路由是登录页，直接放行
+  if (to.path === '/login') {
+    next();
+    return;
+  }
+
+  // 验证 access_token
+  if (accessToken) {
+    try {
+      const response = await verifyToken({ accessToken });
+      if (response.data.success) {
+        next(); // Token 有效，继续跳转
+      } else {
+        next('/login'); // Token 无效，跳转到登录页
+      }
+    } catch (error) {
+      next('/login'); // 验证错误时跳转
+    }
+  } else {
+    next('/login'); // 没有 Token 时跳转到登录页
+  }
+});
+
   export default router
